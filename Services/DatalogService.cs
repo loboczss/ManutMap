9bdfe91ee75ae4506d2278ba6c61fab1393579a4
loboczss <72765585+loboczss@users.noreply.github.com>
2025-07-03 12:44:11 -0500

    Fix file matching when merging maintenance JSONs

--- Services/DatalogService.cs
+++ Services/DatalogService.cs
@@ -28,6 +28,8 @@ namespace ManutMap.Services
         private static readonly string[] JsonSuffixes =
         {
             "__Manutencao_AC2025.json",
+            "__Manutencao_AC2024.json",
+            "__Manutencao_AC2023.json",
             "__Manutencao_MT.json"
         };
 
